32fb5913102ac2b9bfd644e8bb8ef85deec191e0
Santhosh Sampath <santhosh@chinsay.com>
2021-06-11 23:06:14 +0800

    added product static context

---
+++ shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,36 @@
+﻿using Shopping.Client.Models;
+using System.Collections.Generic;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>()
+        {
+            new Product()
+            {
+                Name = "IPhone X",
+                Description = "IPhone X Description",
+                ImageFile = "product1.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "Samsung S20",
+                Description = "Samsung S20 Description",
+                ImageFile = "product2.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "Samsung S10",
+                Description = "Samsung S10 Description",
+                ImageFile = "product3.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            }
+        };
+    }
+}
