4e6e84cad62cb2b0bab6f7c0c73150014cd36d7e
Santhosh Sampath <santhosh@chinsay.com>
2021-06-13 00:15:13 +0800

    link api proj to mongo db on docker

--- shopping/Shopping.API/Controllers/ProductController.cs
+++ shopping/Shopping.API/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Shopping.API.Data;
 using Shopping.API.Models;
 
@@ -15,16 +16,19 @@ namespace Shopping.API.Controllers
     public class ProductController : ControllerBase
     {
         private readonly ILogger<ProductController> _logger;
+        private readonly ProductContext _productContext;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController(ILogger<ProductController> logger,
+            ProductContext productContext)
         {
             _logger = logger;
+            _productContext = productContext;
         }
 
         [HttpGet]
-        public IEnumerable<Product> Get()
+        public async Task<IEnumerable<Product>> Get()
         {
-            return ProductContext.Products;
+            return await _productContext.Products.Find(p => true).ToListAsync();
         }
     }
 }
--- shopping/Shopping.API/Data/ProductContext.cs
+++ shopping/Shopping.API/Data/ProductContext.cs
@@ -1,44 +1,69 @@
-﻿using Shopping.API.Models;
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Shopping.API.Models;
 using System.Collections.Generic;
 
 namespace Shopping.API.Data
 {
-    public static class ProductContext
+    public class ProductContext
     {
-        public static readonly List<Product> Products = new List<Product>()
+        public ProductContext(IConfiguration configuration)
         {
-            new Product()
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+            SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+
+        private static void SeedData(IMongoCollection<Product> productsCollection)
+        {
+            bool existProduct = productsCollection.Find(p => true).Any();
+            if (!existProduct)
             {
-                Name = "IPhone X",
-                Description = "IPhone X Description",
-                ImageFile = "product1.png",
-                Price = 950.00M,
-                Category = "Smart Phone"
-            },
-            new Product()
-            {
-                Name = "Samsung S20",
-                Description = "Samsung S20 Description",
-                ImageFile = "product2.png",
-                Price = 1050.00M,
-                Category = "Smart Phone"
-            },
-            new Product()
-            {
-                Name = "Samsung S10",
-                Description = "Samsung S10 Description",
-                ImageFile = "product3.png",
-                Price = 900.00M,
-                Category = "Smart Phone"
-            },
-            new Product()
-            {
-                Name = "Huawei P20",
-                Description = "Huawei P20 Description",
-                ImageFile = "product4.png",
-                Price = 700.00M,
-                Category = "Smart Phone"
+                productsCollection.InsertMany(GetPreConfiguredProducts());
             }
-        };
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Description = "IPhone X Description",
+                    ImageFile = "product1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung S20",
+                    Description = "Samsung S20 Description",
+                    ImageFile = "product2.png",
+                    Price = 1050.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung S10",
+                    Description = "Samsung S10 Description",
+                    ImageFile = "product3.png",
+                    Price = 900.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Huawei P20",
+                    Description = "Huawei P20 Description",
+                    ImageFile = "product4.png",
+                    Price = 700.00M,
+                    Category = "Smart Phone"
+                }
+            };
+        }
     }
 }
