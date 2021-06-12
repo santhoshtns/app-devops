1f42a86417c23798ec67dc83e7a57434817c190b
Santhosh Sampath <santhosh@chinsay.com>
2021-06-12 19:39:02 +0800

    test ci/cd

--- shopping/Shopping.Client/Data/ProductContext.cs
+++ shopping/Shopping.Client/Data/ProductContext.cs
@@ -20,7 +20,7 @@ namespace Shopping.Client.Data
                 Name = "Samsung S20",
                 Description = "Samsung S20 Description",
                 ImageFile = "product2.png",
-                Price = 950.00M,
+                Price = 1050.00M,
                 Category = "Smart Phone"
             },
             new Product()
@@ -28,7 +28,15 @@ namespace Shopping.Client.Data
                 Name = "Samsung S10",
                 Description = "Samsung S10 Description",
                 ImageFile = "product3.png",
-                Price = 950.00M,
+                Price = 900.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Name = "Huawei P20",
+                Description = "Huawei P20 Description",
+                ImageFile = "product4.png",
+                Price = 700.00M,
                 Category = "Smart Phone"
             }
         };
